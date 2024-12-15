89eb01a9100f5754b139334566e1de493c2758d2
hwk9407 <hwk__@naver.com>
2024-12-15 22:26:25 +0900

    [#1000] test: admin 모듈에 test 코드 추가

---
+++ admin/src/test/java/com/sparta/admin/product/service/ProductServiceTest.java
@@ -0,0 +1,17 @@
+package com.sparta.admin.product.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProductServiceTest {
+
+    @Test
+    @DisplayName("admin 용 테스트")
+    void adminModuleTest1() {
+        int a = 0;
+
+        assertThat(a).isEqualTo(0);
+    }
+}
