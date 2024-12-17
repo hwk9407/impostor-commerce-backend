5392b7dc4bdadd55bac01fcda3b48361786518b0
hwk9407 <hwk__@naver.com>
2024-12-17 19:58:44 +0900

    [#main] fix: deploy.sh에 프로세스가 실행중인 경우에만 kill 명령어를 사용하도록 로직 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # 프로세스가 실행 중이라면 종료
-ps aux | grep 'java -jar' | grep -v grep | awk '{print $2}' | xargs kill -9
+pids=$(ps aux | grep 'java -jar' | grep -v grep | awk '{print $2}')
+if [ -n "$pids" ]; then
+    echo "Killing process with PID: $pids"
+    echo "$pids" | xargs kill -9
+else
+    echo "No java processes found to kill."
+fi
 
 # b2c, b2b, admin 각각의 jar 파일 실행 (배포된 파일 경로를 사용)
 java -jar /opt/impostor/b2c/b2c.jar &
