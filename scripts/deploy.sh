f24b3ecd0996a61be1940cc2baec098a03f7178e
hwk9407 <hwk__@naver.com>
2024-12-17 19:31:10 +0900

    [#main] fix: 와일드카드 대신 정확한 파일명으로 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,8 @@
 ps aux | grep 'java -jar' | grep -v grep | awk '{print $2}' | xargs kill -9
 
 # b2c, b2b, admin 각각의 jar 파일 실행 (배포된 파일 경로를 사용)
-java -jar /opt/impostor/b2c/*.jar &
-java -jar /opt/impostor/b2b/*.jar &
-java -jar /opt/impostor/admin/*.jar &
+java -jar /opt/impostor/b2c/b2c.jar &
+java -jar /opt/impostor/b2b/b2b.jar &
+java -jar /opt/impostor/admin/admin.jar &
 
 echo "Deployment complete!"
